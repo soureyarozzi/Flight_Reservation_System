95fb3086f454d18597e2e7be1fcaaf3ea406da6a
soureyarozzi <104095285+soureyarozzi@users.noreply.github.com>
2023-12-21 22:09:32 +0300

    Controllers

---
+++ Flight_Reservation_System/Flight_Reservation_System/Controllers/AdminController.cs
@@ -0,0 +1,25 @@
+﻿using Flight_Reservation_System.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Flight_Reservation_System.Controllers
+{
+    public class AdminController : Controller
+    {
+        private AgencyContext _context;
+        public AdminController(AgencyContext wsc)
+        {
+            _context = wsc;
+        }
+        [Authorize(Roles = "Admin")]
+        //public IActionResult Index()
+        //{
+        //    Admin admin = new Admin();
+        //    admin.toursCount = _context.Tours.Count();
+        //    admin.countriesCount = _context.Countries.Count();
+        //    admin.passengersCount = _context.Users.Count();
+        //    admin.citiesCount = _context.Cities.Count();
+        //    return View(admin);
+        //}
+    }
+}
---
+++ Flight_Reservation_System/Flight_Reservation_System/Controllers/BookingsController.cs
@@ -0,0 +1,129 @@
+﻿using Flight_Reservation_System.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Flight_Reservation_System.Controllers
+{
+    public class BookingsController : Controller
+    {
+
+    //    private readonly AgencyContext _context;
+
+    //    public BookingsController(AgencyContext context, UserManager<User> user)
+    //    {
+    //        _context = context;
+    //    }
+
+    //    // GET: Bookings
+    //    [Authorize]
+    //    public async Task<IActionResult> Index(string id)
+    //    {
+    //        var bookings = await _context.Bookings.Where(b => b.customerId == TempData["userId"]).ToListAsync();
+    //        foreach (var b in bookings)
+    //        {
+    //            var t = await _context.Tours.Where(t => t.TourId == b.TourId).FirstOrDefaultAsync();
+    //            b.tour = t;
+    //        }
+    //        return View(bookings);
+    //    }
+
+    //    // GET: Bookings/Details/5
+    //    [Authorize]
+    //    public async Task<IActionResult> Details(int? id)
+    //    {
+    //        if (id == null)
+    //        {
+    //            return NotFound();
+    //        }
+
+    //        var booking = await _context.Bookings
+    //            .FirstOrDefaultAsync(m => m.BookingId == id);
+    //        var user = _context.Users.FirstOrDefault(x => x.Id == booking.customerId);
+    //        var tour = _context.Tours.FirstOrDefault(x => x.TourId == booking.TourId);
+    //        booking.customer = user;
+    //        booking.tour = tour;
+    //        if (booking == null)
+    //        {
+    //            return NotFound();
+    //        }
+    //        return View(booking);
+    //    }
+
+    //    // GET: Bookings/Create
+    //    public IActionResult Create()
+    //    {
+    //        return View();
+    //    }
+
+    //    // POST: Bookings/Create
+    //    // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+    //    // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+    //    [Authorize]
+    //    [HttpPost]
+    //    [ValidateAntiForgeryToken]
+    //    public async Task<IActionResult> Create([Bind("BookingId,customerId,TourId,placed_at")] Booking booking)
+    //    {
+    //        if (ModelState.IsValid)
+    //        {
+    //            booking.state = "Unconfirmed";
+    //            _context.Add(booking);
+    //            await _context.SaveChangesAsync();
+    //            TempData["BookingId"] = booking.BookingId;
+    //            TempData["error"] = null;
+    //            return RedirectToAction("Authenticate", "CreditCards");
+    //        }
+    //        return View(booking);
+    //    }
+
+
+    //    // GET: Bookings/Delete/5
+    //    public async Task<IActionResult> Delete(int? id)
+    //    {
+    //        if (id == null)
+    //        {
+    //            return NotFound();
+    //        }
+
+    //        var booking = await _context.Bookings
+    //            .FirstOrDefaultAsync(m => m.BookingId == id);
+    //        var tour = await _context.Tours.FirstOrDefaultAsync(t => t.TourId == booking.TourId);
+    //        booking.tour = tour;
+    //        var pay = _context.Payments.FirstOrDefault(x => x.PaymentId == booking.PaymentId);
+    //        if (pay != null)
+    //        {
+    //            TempData["isPayedDate"] = pay.payment_date;
+    //        }
+    //        else
+    //        {
+    //            TempData["isPayedDate"] = null;
+    //        }
+    //        if (booking == null)
+    //        {
+    //            return NotFound();
+    //        }
+
+    //        return View(booking);
+    //    }
+
+    //    // POST: Bookings/Delete/5
+    //    [HttpPost, ActionName("Delete")]
+    //    [ValidateAntiForgeryToken]
+    //    public async Task<IActionResult> DeleteConfirmed(int id)
+    //    {
+
+    //        var booking = await _context.Bookings.FindAsync(id);
+    //        var pay = _context.Payments.FirstOrDefault(x => x.PaymentId == booking.PaymentId);
+    //        if (pay != null) _context.Payments.Remove(pay);
+    //        _context.Bookings.Remove(booking);
+    //        await _context.SaveChangesAsync();
+    //        return RedirectToAction(nameof(Index));
+    //    }
+
+    //    private bool BookingExists(int id)
+    //    {
+    //        return _context.Bookings.Any(e => e.BookingId == id);
+    //    }
+    }
+}
