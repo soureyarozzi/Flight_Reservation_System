f74e208fc1d7b0d5c976b49ae109104ab172b790
linaalsawadi <104251267+linaalsawadi@users.noreply.github.com>
2023-12-23 06:11:44 +0300

    Authorization

---
+++ Flight_Reservation_System/Flight_Reservation_System/Migrations/20231223003903_suri.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Flight_Reservation_System.Migrations
+{
+    /// <inheritdoc />
+    public partial class suri : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
