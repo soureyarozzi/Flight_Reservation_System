5929c60c6336571bb426b51a49a63f35fb3dd09c
linaalsawadi <104251267+linaalsawadi@users.noreply.github.com>
2023-12-29 23:56:45 +0300

    AirPlane.cs

--- Flight_Reservation_System/Flight_Reservation_System/Models/AirPlane.cs
+++ Flight_Reservation_System/Flight_Reservation_System/Models/AirPlane.cs
@@ -1,11 +1,16 @@
-﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations;
 
 namespace Flight_Reservation_System.Models
 {
     public class AirPlane
     {
         [Key]
+        [Required]
         public int PlaneId { get; set; }
+        public string PlaneName { get; set; }
         public int Capacity { get; set; }
+        
+        public ICollection<Flights>? Flights { get; set; }//Navigation her Flights birden fazla Airplani olabilir
+
     }
 }
